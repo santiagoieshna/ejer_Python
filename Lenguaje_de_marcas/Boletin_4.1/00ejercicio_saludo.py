c39d863dd7a91f5d3b7d207fd20920e6708c40e4
xtalo <117736097+santiagoieshna@users.noreply.github.com>
2023-02-26 10:38:26 +0100

    Create 00ejercicio_saludo.py

---
+++ Lenguaje_de_marcas/Boletin_4.1/00ejercicio_saludo.py
@@ -0,0 +1,24 @@
+"""
+print("hola sabrina")
+entero = 2
+print()
+print("Mi entero es",entero)
+print(type(entero))
+print("Mi entero es "+ str(entero))
+
+decimal = 2.33
+print(type(decimal))
+print (entero+decimal)
+"""
+listas = [2, 3, 1, 3.4, "Hola :D"] # lista.lenght()
+print("longitud de la lista",len(listas))
+print(listas)
+print(listas[0])
+print(listas[1])
+print(listas[2])
+print(listas[3])
+
+elementos = [1 , 2 , 5 ,-78 , 34.98 , "Sabrina", "Peralta"]
+
+for elemento in elementos:
+    print(elemento)
